9715e1b1af3d5a9c6111dbc759135f6a95139538
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-04-25 10:12:32 +0200

    added app

--- my-first-app/app.R
+++ my-first-app/app.R
@@ -1,9 +1,13 @@
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
 
 
-    titlePanel("App")
+    titlePanel("AppAAAAAAAAA"),
+    sliderInput("slider1", "Slide sth", min = 1, max = 100, 
+                value = c(4, 5)),
+    plotOutput("plot1")
 
     
 )
@@ -11,6 +15,14 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
+    output[["plot1"]] <- renderPlot({
+        df <- data.frame(x = input[["slider1"]][1]:input[["slider1"]][2],
+                         y = 4)
+        ggplot(df, aes(x = x, y = y)) +
+            geom_point()
+    })
+    
+    
 }
 
 shinyApp(ui = ui, server = server)
