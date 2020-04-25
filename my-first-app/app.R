8cac9c57f7f394d386c685425614c439dc24fa23
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-04-25 10:20:32 +0200

    added reactive object

--- my-first-app/app.R
+++ my-first-app/app.R
@@ -1,13 +1,17 @@
 library(shiny)
 library(ggplot2)
 
+# https://github.com/rstudio/reactlog
+
 ui <- fluidPage(
 
 
     titlePanel("AppAAAAAAAAA"),
+    textInput("text1", "Enter title", value = "title"),
     sliderInput("slider1", "Slide sth", min = 1, max = 100, 
                 value = c(4, 5)),
-    plotOutput("plot1")
+    textOutput("text_value"),
+    plotOutput("plot1", click = "plot1_click")
 
     
 )
@@ -15,14 +19,21 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-    output[["plot1"]] <- renderPlot({
-        df <- data.frame(x = input[["slider1"]][1]:input[["slider1"]][2],
-                         y = 4)
-        ggplot(df, aes(x = x, y = y)) +
-            geom_point()
+    
+    df <- reactive({
+        data.frame(x = input[["slider1"]][1]:input[["slider1"]][2],
+                   y = 4)
     })
     
+    output[["plot1"]] <- renderPlot({
+        ggplot(df(), aes(x = x, y = y)) +
+            geom_point() +
+            ggtitle(input[["text1"]])
+    })
     
+    output[["text_value"]] <- renderText({
+        paste0("Title is ", input[["text1"]])
+    })
 }
 
 shinyApp(ui = ui, server = server)
